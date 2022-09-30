4e50b6874e5ea764f24a81c04af2860f7a3cea45
Dominik Dumont <dominik.dumont@aoe.com>
2022-09-30 13:26:34 +0200

    feat: DPT5.003 goes uint16 to avoid superfluous rounding issues

--- knx/dpt/types_5_test.go
+++ knx/dpt/types_5_test.go
@@ -1,8 +1,6 @@
 package dpt
 
 import (
-	"math"
-	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,32 +22,16 @@ func TestDPT_5001(t *testing.T) {
 
 // Test DPT 5.003 (Angle) with values within range
 func TestDPT_5003(t *testing.T) {
-	var buf []byte
-	var src, dst DPT_5003
+	knxValue := []byte{0, 30}
+	dptValue := DPT_5003(42)
 
-	// Calculate the quantization error we expect
-	const Q = float32(360) / 255
+	var tmpDPT DPT_5003
+	assert.NoError(t, tmpDPT.Unpack(knxValue))
+	assert.Equal(t, dptValue, tmpDPT)
 
-	for i := 1; i <= 10; i++ {
-		value := rand.Float32()
+	assert.Equal(t, knxValue, dptValue.Pack())
 
-		// Scale the random number to the given range
-		value *= 360
-
-		// Pack and unpack to test value
-		src = DPT_5003(value)
-		if abs(float32(src)-value) > epsilon {
-			t.Errorf("Assignment of value \"%v\" failed for source of type DPT_5003! Has value \"%s\".", value, src)
-		}
-		buf = src.Pack()
-		dst.Unpack(buf)
-		if math.IsNaN(float64(dst)) {
-			t.Errorf("Value \"%s\" is not a valid number! Original value was \"%v\".", dst, value)
-		}
-		if abs(float32(dst)-value) > (Q + epsilon) {
-			t.Errorf("Value \"%s\" after pack/unpack above quantization noise! Original value was \"%v\", noise is \"%f\"", dst, value, Q)
-		}
-	}
+	assert.Equal(t, "42°", dptValue.String())
 }
 
 func TestDPT_5005(t *testing.T) {
